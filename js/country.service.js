992fc90706d48515eadf30ebe94287e970d0979b
eranyosef2 <eranyosefhd@gmail.com>
2024-10-06 17:47:28 +0300

    Added caching mechanism

--- js/country.service.js
+++ js/country.service.js
@@ -1,5 +1,27 @@
 'use strict'
 
 function getCountryByName(name) {
+    const cache = getCache()
+    
+    if (cache[name]) {
+        console.log(`Serving ${name} from cache`)
+        return Promise.resolve(cache[name])
+    }
+
     return axios.get(`https://restcountries.com/v3.1/name/${name}`)
+        .then(response => {
+            const countryData = response.data[0]
+            cache[name] = countryData
+            saveCache(cache)
+            return countryData
+        })
+}
+
+function getCache() {
+    const cache = localStorage.getItem('countryCache')
+    return cache ? JSON.parse(cache) : {}
+}
+
+function saveCache(cache) {
+    localStorage.setItem('countryCache', JSON.stringify(cache))
 }
